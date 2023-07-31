0bc3e1a048ebc4e7d75e1a9614eecd4ea0eb1d14
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-31 14:30:11 +0800

    Add files via upload

---
+++ check_leap_year.py
@@ -0,0 +1,12 @@
+#python program to check if a Number is odd or even
+
+
+n = int(input("Enter the year : "))
+
+year = n % 4
+
+if year == 0:
+   print ("n is leap year")
+
+else:
+   print ("n is not leap year")
